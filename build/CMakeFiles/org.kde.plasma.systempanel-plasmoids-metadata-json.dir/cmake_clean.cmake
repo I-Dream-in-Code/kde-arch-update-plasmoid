b2b153104150207f2aa7de2c5e293829a49e5e3a
Mike Harris <mike.a.harris@outlook.com>
2017-06-21 11:52:27 -0700

    fix icon size

--- build/CMakeFiles/org.kde.plasma.systempanel-plasmoids-metadata-json.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/org.kde.plasma.systempanel-plasmoids-metadata-json"
-  "org.kde.plasma.systempanel-plasmoids-metadata.json"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/org.kde.plasma.systempanel-plasmoids-metadata-json.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
