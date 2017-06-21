bfa509c1f8b74d613636148e17aa67f13229c36b
Mike Harris <mike.a.harris@outlook.com>
2017-06-21 15:10:29 -0700

    fix plasmoid installer

--- install_plasmoid.sh
+++ install_plasmoid.sh
@@ -4,7 +4,8 @@ mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DLIB_INSTALL_DIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
 sudo make install
-rm -rf build/
+cd ..
+sudo rm -rf build
 killall plasmashell
 kstart plasmashell
 
