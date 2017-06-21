643e395ae991b13ab25fd78c87284e03e6e90f32
Mike Harris <mike.a.harris@outlook.com>
2017-06-20 20:17:01 -0700

    fix contrast

--- build/CMakeFiles/org.kde.archUpdate-plasmoids-metadata-json.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/org.kde.archUpdate-plasmoids-metadata-json"
-  "org.kde.archUpdate-plasmoids-metadata.json"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/org.kde.archUpdate-plasmoids-metadata-json.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
