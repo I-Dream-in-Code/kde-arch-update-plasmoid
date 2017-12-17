f1b250502d7c57804da3bcad2145e113d30435b0
Mike Harris <mike.a.harris@outlook.com>
2017-12-17 11:47:05 -0800

    remove kdev project dirs

--- build/plugin/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mike/Dropbox/repos/kde-arch-update-plasmoid/plugin
-# Build directory: /home/mike/Dropbox/repos/kde-arch-update-plasmoid/build/plugin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
