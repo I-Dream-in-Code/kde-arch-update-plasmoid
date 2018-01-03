075b60a88ac84c68d2fa62bf72eda80c1ac64c65
Mike Harris <mike.a.harris@outlook.com>
2018-01-03 07:11:52 -0800

    fixed cmake deprecation

--- install_plasmoid.sh
+++ install_plasmoid.sh
@@ -2,7 +2,7 @@
 rm -rf build
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DLIB_INSTALL_DIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_LIBDIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
 sudo make install
 cd ..
 sudo rm -rf build
